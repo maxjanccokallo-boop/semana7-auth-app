3ca9d7408f48ee7f2745fcf0403339443bf804e2
max antony janccokallo <izumi>
2026-05-06 15:52:15 -0500

    nuevo

--- react-auth-app/server.js
+++ react-auth-app/server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // React SPA
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
