a7d5ce6f7c34bfefb6c91c3d0df7d820342350a2
max antony janccokallo <izumi>
2026-05-06 15:48:55 -0500

    frontend webservice

---
+++ react-auth-app/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir archivos de Vite build
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React SPA
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
